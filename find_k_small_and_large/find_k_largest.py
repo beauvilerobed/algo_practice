2cd8a561eaab24bcdc04e12a605fa0be052e7b20
Robed <robedbeauvil@gmail.com>
2020-11-09 13:20:24 -0500

    added find k largest and tests, come back to it

---
+++ find_k_small_and_large/find_k_largest.py
@@ -0,0 +1,36 @@
+from heapq import heappop, heapify
+import sys
+
+def k_largest_naive(nums, k):
+    nums = set(nums)
+    nums = list(nums)
+    if len(nums) < k:
+        return None
+        
+    nums.sort(reverse=True)
+    return nums[k - 1]
+
+def k_largest(nums, k):
+    nums = set(nums)
+    nums = list(nums)
+    nums = [-num for num in nums]
+    heapify(nums)
+    
+    if k > len(nums):
+        return None
+
+    k_max = 0
+    for _ in range(k):
+        k_max = heappop(nums)
+    
+    return -k_max
+
+def main():
+    k = int(sys.stdin.readline())
+    data = sys.stdin.readline()
+    values = list(map(int, data.rstrip().split()))
+    print(k_largest(values, k))
+
+
+if __name__ == '__main__':
+    main()
---
+++ find_k_small_and_large/tests.py
@@ -0,0 +1,59 @@
+import unittest
+import random
+from find_k_smallest import k_smallest, k_smallest_naive
+from find_k_largest import k_largest, k_largest_naive
+
+
+class KSmallestAndLargest(unittest.TestCase):
+    def test_small_k_smallest(self):
+        test_cases = [
+            ([1, 1, 1], 2, None),
+            ([9, 8, 7, 6, 5, 4, 3, 2, 1], 3, 3),
+            ([1, 1, 3, 4], 2, 3),
+            ([], 100, None)
+        ]
+        for nums, k, answer in test_cases:
+            self.assertEqual(k_smallest(nums, k), answer)
+    
+    def test_small_k_largest(self):
+        test_cases = [
+            ([1, 1, 1], 2, None),
+            ([9, 8, 7, 6, 5, 4, 3, 2, 1], 3, 7),
+            ([1, 1, 3, 4], 2, 3),
+            ([], 100, None)
+        ]
+        for nums, k, answer in test_cases:
+            self.assertEqual(k_largest(nums, k), answer)
+        
+    def test_large_k_smallest(self):
+        test_cases = [
+            ([1] * 10 ** 5, 2, None),
+            ([i for i in range(10 ** 5 + 1)], 10 ** 5 , 10 ** 5 - 1),
+            ([(i + 1) % 10 for i in range(10 ** 5)], 5, 4)
+        ]
+        for nums, k, answer in test_cases:
+            self.assertEqual(k_smallest(nums, k), answer)
+    
+    def test_large_k_largest(self):
+        test_cases = [
+            ([1] * 10 ** 5, 2, None),
+            ([i for i in range(10 ** 5 + 1)], 10 ** 5 , 1),
+            ([(i + 1) % 10 for i in range(10 ** 5)], 5, 5)
+        ]
+        for nums, k, answer in test_cases:
+            self.assertEqual(k_largest(nums, k), answer)
+
+    def test_stress_k_smallest(self):
+        test_cases = [([random.randint(1, 1000) for _ in range(random.randint(50, 100))], random.randint(50, 100)) for _ in range(1000)]
+        for nums, k in test_cases:
+            self.assertEqual(k_smallest(nums, k), k_smallest_naive(nums, k))
+
+    def test_stress_k_largest(self):
+        test_cases = [([random.randint(1, 1000) for _ in range(random.randint(50, 100))], random.randint(50, 100)) for _ in range(1000)]
+        for nums, k in test_cases:
+            self.assertEqual(k_largest(nums, k), k_largest_naive(nums, k))
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
