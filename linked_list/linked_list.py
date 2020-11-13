fe8cb3472369eb04eb7dcb311e6b6932f263bf60
Robed <robedbeauvil@gmail.com>
2020-11-13 10:42:05 -0500

    fixed bug for linked lists and added new method

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -12,6 +12,8 @@ class LinkedList:
         self.length = 1 if head is not None else 0
 
     def __repr__(self):
+        if self.head is None:
+            return ''
         node = self.head
         linkedlist = str(node.value)
         while node.next:
@@ -19,6 +21,18 @@ class LinkedList:
             linkedlist += '->' + str(node.value)
 
         return linkedlist
+    
+    def return_array(self):
+        if self.head is None:
+            return []
+        array = []
+        node = self.head
+        array.append(node.value)
+        while node.next:
+            node = node.next
+            array.append(node.value)
+
+        return array
     
     def __len__(self):
         return self.length
@@ -29,6 +43,34 @@ class LinkedList:
         self.head = node
         self.length += 1
 
+    def remove(self, n):
+        """
+            remove method deletes elements based on their 
+            indexes or position in the linked list
+        """
+        if n < 0 or n >= self.length - 1 or self.head is None:
+            return "nothing to remove"
+        
+        count = 0
+        node = self.head
+        prev = None
+
+        while count < n:
+            prev = node
+            node = node.next
+            count += 1
+
+        # this indicates that query was the head value
+        if prev is None:
+            prev = node.next
+            node.next = None
+            self.head = prev
+        
+        else:
+            prev.next = node.next
+            node.next = None
+
+
     def search(self, query):
         node = self.head
         
@@ -38,17 +80,36 @@ class LinkedList:
                 return "success"
 
         return "failure"
+    
+    def search_nth(self, n):
+        if n < 0 or n >= self.length - 1 or self.head is None:
+            return "doesn't exist"
+        
+        count = 0
+        node = self.head
+
+        while count < n:
+            node = node.next
+            count += 1
+        
+        return node.value
 
     def middle(self):
+        if self.head is None:
+            return "empyt linked list"
+
         fast = self.head
         slow = self.head
-        while fast.next:
+        while fast.next and fast.next.next:
             fast = fast.next.next
             slow = slow.next
 
         return slow.value
 
     def reverse(self):
+        if self.head is None:
+            return "empty linked list"
+
         node = self.head
         prev = None
 
@@ -64,6 +125,7 @@ class LinkedList:
 
 
 def main():
+
     data = sys.stdin.readline()
     data = list(map(int, data.split()))
     linkedlist = LinkedList()
@@ -71,14 +133,41 @@ def main():
         linkedlist.add(val)
 
     print(linkedlist)
+    print(linkedlist.return_array())
+
     linkedlist.reverse()
+
     print(linkedlist)
+    print(linkedlist.return_array())
     print("The length of the linked list is: ", len(linkedlist))
+
     value = int(input("pick a number to search: "))
+
     print(linkedlist.search(value))
-    print("the middle of the linked list is: ",linkedlist.middle())
-    linkedlist.reverse()
+    print("the middle of the linked list is: ", linkedlist.middle())
+
+    val = linkedlist.middle()
+
     print(linkedlist)
+
+    val = linkedlist.middle()
+
+    print(linkedlist)
+    print("pop")
+    linkedlist.remove(0)
+
+    print(linkedlist)
+    print("pop")
+
+    linkedlist.remove(0)
+
+    print(linkedlist)
+    print("the first element is:", linkedlist.search_nth(0))
+
+    linkedlist.reverse()
+
+    print(linkedlist)
+    print("the 12-th element is:", linkedlist.search_nth(11))
 
 
 if __name__ == '__main__':
--- linked_list/tests.py
+++ linked_list/tests.py
@@ -3,26 +3,31 @@ import random
 from linked_list import LinkedList
 
 
-def generate_linkedlists_and_arrays(number=1000):
+def generate_linkedlists_and_arrays(number=500):
     arrays = [[random.randint(1, number) for _ in range(1, number)] for _ in range(number)]
     linklists = []
     for nums in arrays:
         linklist = LinkedList()
-        for num in nums:
-            linklist.add(num)
+        n = len(nums)
+        for i in range(n):
+            linklist.add(nums[n-i-1])
+        linklists.append(linklist)
 
     return linklists, arrays
 
 class TestLinkedList(unittest.TestCase):
     def test_reverse_method(self):
-        linklist, arrays = generate_linkedlists_and_arrays()
-        for linklist, nums in zip(linklist, arrays):
-            self.assertEqual(linklist.reverse(), nums)
+        linklists, arrays = generate_linkedlists_and_arrays()
+        for linklist, nums in zip(linklists, arrays):
+            self.assertEqual(linklist.return_array(), nums)
 
     def test_search_method(self):
-        linklist, arrays = generate_linkedlists_and_arrays()
+        linklists, arrays = generate_linkedlists_and_arrays()
+
+        assert len(linklists) > 0 and len(arrays) > 0
+
         n = random.randint(1, 1000)
-        for linklist, nums in zip(linklist, arrays):
+        for linklist, nums in zip(linklists, arrays):
             result = ''
             if n in nums:
                 result = "success"
@@ -31,15 +36,43 @@ class TestLinkedList(unittest.TestCase):
             self.assertEqual(linklist.search(n), result)
 
     def test_len_method(self):
-        linklist, arrays = generate_linkedlists_and_arrays()
-        for linklist, nums in zip(linklist, arrays):
+        linklists, arrays = generate_linkedlists_and_arrays()
+
+        assert len(linklists) > 0 and len(arrays) > 0
+
+        for linklist, nums in zip(linklists, arrays):
             self.assertEqual(len(linklist), len(nums))
 
     def test_middle_method(self):
-        linklist, arrays = generate_linkedlists_and_arrays()
-        for linklist, nums in zip(linklist, arrays):
+        linklists, arrays = generate_linkedlists_and_arrays()
+
+        assert len(linklists) > 0 and len(arrays) > 0   
+
+        for linklist, nums in zip(linklists, arrays):
+            m = len(nums)
+            self.assertEqual(linklist.middle(), nums[m // 2])
+
+    def test_remove_method(self):
+        linklists, arrays = generate_linkedlists_and_arrays()
+
+        assert len(linklists) > 0 and len(arrays) > 0  
+
+        for linklist, nums in zip(linklists, arrays):
             n = len(nums)
-            self.assertEqual(linklist.middle(), nums[n // 2 - 1])
+            del nums[n // 2 - 1]
+            linklist.remove(n // 2 - 1)
+            self.assertEqual(linklist.return_array(), nums)
+
+    def test_search_nth_method(self):
+        linklists, arrays = generate_linkedlists_and_arrays()
+
+        assert len(linklists) > 0 and len(arrays) > 0  
+
+        for linklist, nums in zip(linklists, arrays):
+            n = len(nums)
+            m = random.randint(-n, -n) 
+            result = nums[m] if 0 <= m and m <= n -1 else "doesn't exist"
+            self.assertEqual(linklist.search_nth(m), result)
 
 
 if __name__ == '__main__':
