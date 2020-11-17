84f47a7b185d6043766e79033602fa98b02e366d
Robed <robedbeauvil@gmail.com>
2020-11-17 11:22:34 -0500

    added heap sort and tests

---
+++ heap_sort/heap_sort.py
@@ -0,0 +1,49 @@
+import sys
+
+
+def heap_sort(nums):
+
+    def heap_sort_util(n):
+        # this will build the heap from nums
+        half = (n - 1) // 2
+        for i in range(half, n):
+            sift_down(n - i - 1, n - 1)
+
+        size = n - 1
+        while size >= 1:
+            nums[0], nums[size] = nums[size], nums[0]
+            size = size - 1
+            sift_down(0, size)
+
+    def sift_down(index, size):
+        
+        while index < size:
+            max_index = index
+            left_child = 2 * index + 1
+            right_child = 2 * index + 2
+
+            if left_child <= size and nums[max_index] <= nums[left_child]:
+                max_index = left_child
+
+            if right_child <= size and nums[max_index] <= nums[right_child]:
+                max_index = right_child
+            
+            if max_index == index:
+                break
+
+            nums[max_index], nums[index] = nums[index], nums[max_index]
+            index = max_index
+
+    n = len(nums)
+    heap_sort_util(n)
+    return nums
+
+
+def main():
+    data = sys.stdin.readline()
+    nums = list(map(int, data.split()))
+    print(heap_sort(nums))
+
+
+if __name__ == '__main__':
+    main()
