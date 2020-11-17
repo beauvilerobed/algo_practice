4c082c2c48d6dd2432b871e8fca764b94f766b87
Robed <robedbeauvil@gmail.com>
2020-11-17 17:03:40 -0500

    added tests to find_cycle

--- find_cycle/tests.py
+++ find_cycle/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from is_cycle import LinkedList, is_cycle
+from find_cycle import find_cycle
 import random
 
 
@@ -20,10 +21,29 @@ class FindCycle(unittest.TestCase):
                 linklist.add_cycle()
 
             actual = is_cycle(linklist)
-            if actual != answer:
-                print(nums)
-                print(linklist)
-            self.assertEqual(is_cycle(linklist), answer)
+            self.assertEqual(actual, answer)
+
+    def test_find_cycle(self):
+        test_cases = [[random.randint(-1000, 1000) for _ in range(random.randint(1, 100))] for _ in range(random.randint(1, 100))]
+        for nums in test_cases:
+            nums_set = set(nums)
+            yes = 0
+            no = -1
+            answer = no if len(nums) == len(nums_set) else yes
+            expected = nums[0]
+
+            linklist = LinkedList()
+
+            for i in range(len(nums)):
+                linklist.add(nums[i])
+
+            if answer == yes:
+                linklist.add_cycle()
+            else:
+                expected = 'no cycle'
+
+            actual = find_cycle(linklist)
+            self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
