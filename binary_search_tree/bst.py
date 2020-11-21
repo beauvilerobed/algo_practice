989dd2b6d254886044f012dc0f4fc2c183866200
Robed <robedbeauvil@gmail.com>
2020-11-20 20:42:37 -0500

    added delete method

--- binary_search_tree/bst.py
+++ binary_search_tree/bst.py
@@ -1,8 +1,11 @@
 import sys
+import random
+
 
 class Node:
     def __init__(self, value):
         self.value = value
+        self.parent = None
         self.left = None
         self.right = None
     
@@ -37,14 +40,43 @@ class BST:
             self.root = node
         
         else:
-            found_node = self.helper_method(self.root, value)
+            found_node = self.helper_method(value)
             if found_node.value > value:
                 found_node.left = node
-            
+                node.parent = found_node
             else:
                 found_node.right = node
+                node.parent = found_node
     
-    def helper_method(self, node, value):
+    def delete(self, value):
+        if self.root == None:
+            return None
+
+        node_to_replace = self.helper_method(value, find=True)
+        print(node_to_replace)
+        node_to_replace_with = self.next(node_to_replace)
+        print(node_to_replace_with)
+
+        parent = node_to_replace_with.parent
+
+        if parent == node_to_replace:
+            node_to_replace.value = node_to_replace_with.value
+            if node_to_replace.right:
+                node_to_replace.right = node_to_replace_with.right
+            else:
+                node_to_replace.left = node_to_replace_with.left
+        else:
+            node_to_replace.value = node_to_replace_with.value
+            if node_to_replace.right:
+                parent.left = node_to_replace_with.right
+            else:
+                parent.right = node_to_replace_with.left
+
+
+    def helper_method(self, value, find=False):
+        
+        node = self.root
+
         while node.left or node.right:
             if value < node.value:
                 if node.left is None:
@@ -53,6 +85,9 @@ class BST:
                     node = node.left
 
             if value >= node.value:
+                if find and node.value == value:
+                    return node
+
                 if node.right is None:
                     return node
                 else:
@@ -60,16 +95,39 @@ class BST:
 
         return node
 
+    def next(self, node):
+        if node is None:
+            return None
+
+        if node.right:
+            node = node.right
+            while node.left:
+                node = node.left
+
         
-    # def delete(self, value):
+        elif node.left:
+            node = node.left
+            while node.right:
+                node = node.right
+
+
+        return node
+
+
 
 def main():
     data = sys.stdin.readline()
     nums = list(map(int, data.split()))
     bst = BST()
+    length = len(nums) - 1
+    random_index = random.randint(0, length)
     for num in nums:
         bst.insert(num)
 
+    print(bst)
+    val = nums[random_index]
+    print(val)
+    bst.delete(val)
     print(bst)
 
 
