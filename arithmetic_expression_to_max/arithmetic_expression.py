4d925b29121c3f4a03cfe4546f0022909083c30c
beauvilerobed <robedbeauvil@gmail.com>
2022-01-25 20:41:10 -0500

    docs: changes to documentation

--- arithmetic_expression_to_max/arithmetic_expression.py
+++ arithmetic_expression_to_max/arithmetic_expression.py
@@ -2,7 +2,7 @@
 
 # Parenthesize an arithmetic expression to maxi- mize its value.
 
-# Input. A string s = s0s1 ⋯ s2n of length at most 29. Each symbol 
+# Input. A string s = s_0s_1s_2...s_2n of length at most 29. Each symbol 
 # at an even position of s is a digit (that is, an integer from 
 # 0 to 9) while each symbol at an odd position is one of three 
 # operations from {+,-,×}.
